2a223927ec52b95a3db7aa6dfe552db0438ac339
Oleg Tsibulevsky <olegt@gini-apps.com>
2018-09-27 12:33:28 +0300

    master - create project

---
+++ Pods/Target Support Files/Pods-RxDemoApp/Pods-RxDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_RxDemoApp
+@end
---
+++ RxDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxDemoApp
+//
+//  Created by Oleg Tsibulevsky on 27/09/2018.
+//  Copyright © 2018 Oleg Tsibulevsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
